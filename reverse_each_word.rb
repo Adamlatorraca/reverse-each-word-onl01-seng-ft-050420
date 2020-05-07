df70eb2c31ab1eaea51c752f4f157fef14c56995
Adam LaTorraca <adam.latorraca@gmail.com>
2020-05-07 15:24:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   original = string.split
   reversed = []
-  array.each do |normal|
+  original.each do |normal|
     reversed  << original[0..10].reverse
   end
 end
